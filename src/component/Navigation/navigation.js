06746535b3804a83f42e79ff5da4eb387b237523
Lucas Savy <lucas.savy@tpg13.cpe.lan>
2022-12-07 15:27:15 +0100

    user components

--- src/component/Navigation/navigation.js
+++ src/component/Navigation/navigation.js
@@ -1,29 +1,39 @@
+import React, { useState } from 'react';
+
 import {UserMoneyDisplay} from "./User/components/UserMoneyDisplay"
 import {UserShortDisplay} from "./User/components/UserShortDisplay"
+import {User} from './User/containers/User'
 
 export const Navigation = (props)=>{
-
+    const [currentUser,setCurrentUser]= useState({
+        img:'https://www.nicepng.com/png/full/982-9820051_heart-2352306885-deadpool-png.png',
+        login:"TOTO",
+        pwd:"oui",
+        money:1000,
+    
+    });
     return (
         <><div>
-            <UserMoneyDisplay
-                id={props.id}
-                surname={props.surname}
-                lastname={props.lastname}
-                login={props.login}
-                pwd={props.pwd}
-                money={props.money}
-            ></UserMoneyDisplay>
+            <User id={currentUser.id}
+                      login={currentUser.login}
+                      pwd={currentUser.pwd}
+                      money={currentUser.money}
+                      img={currentUser.img}
+                      display_type='MONEY'>
+            </User>
+
+
         </div><div>
 
             </div><div>
-                <UserShortDisplay
-                    id={props.id}
-                    surname={props.surname}
-                    lastname={props.lastname}
-                    login={props.login}
-                    pwd={props.pwd}
-                    money={props.money}
-                ></UserShortDisplay>
+            <User id={currentUser.id}
+                      login={currentUser.login}
+                      pwd={currentUser.pwd}
+                      money={currentUser.money}
+                      img={currentUser.img}
+                      display_type='SHORT'>
+              </User>
+
             </div></>
 
     );
